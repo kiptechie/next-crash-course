7cb418c4e9afbbeda5a81c0227eca034842120f0
Timothy Serem <theecodepoet@gmail.com>
2022-05-04 19:10:27 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://jsonplaceholder.typicode.com/posts?_limit=0";
+  : "https://next-crash-course-ruddy.vercel.app";
